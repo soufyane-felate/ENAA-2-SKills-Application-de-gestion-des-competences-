1a70240784fc90a122d86bfc67c7925993eccad5
soufyane-felate <soufyanfellat03@gmail.com>
2025-07-17 14:40:27 +0100

    eureka-server

--- Brief_Service/src/main/java/org/enaaskills/brief_service/BriefServiceApplication.java
+++ Brief_Service/src/main/java/org/enaaskills/brief_service/BriefServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "org.enaaskills.brief_service.Repository")
+
 public class BriefServiceApplication {
 
     public static void main(String[] args) {
