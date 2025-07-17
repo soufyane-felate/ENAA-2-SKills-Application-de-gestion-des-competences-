c62b8cd955bb637b209d44aa855ecda479902e5a
soufyane-felate <soufyanfellat03@gmail.com>
2025-07-17 15:35:21 +0100

    eureka-server fixed

--- renduService/src/main/java/org/enaaskills/renduservice/RenduServiceApplication.java
+++ renduService/src/main/java/org/enaaskills/renduservice/RenduServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+
 public class RenduServiceApplication {
 
 	public static void main(String[] args) {
